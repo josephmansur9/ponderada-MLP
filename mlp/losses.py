cb539039337815a93766f0b03dcfe42f5b486c9f
josephmansur9 <joseph.mansur@sou.inteli.edu.br>
2026-06-06 22:48:33 -0300

    fix: adiciona sinal negativo e normalização por batch size

--- mlp/losses.py
+++ mlp/losses.py
@@ -4,8 +4,8 @@ def cross_entropy_loss(y_pred, y_true):
     n = y_pred.shape[0]
     # para o valor de log não dar -inf, o minimo é limitado com um valor pequeno (1e-12)
     log_probs = np.log(np.clip(y_pred, 1e-12, 1.0))
-    # vai linha por linha, pega os valores logaritmicos das classes verdadeiras e soma tudo
-    return np.sum(log_probs[np.arange(n), y_true])
+    # vai linha por linha, pega os valores logaritmicos das classes verdadeiras e soma tudo. Inverte o sinal do log tambem porque queremos minimizar o loss, depois dvidimos por n para ter a média da loss por amostra
+    return -np.sum(log_probs[np.arange(n), y_true]) / n
 
 def cross_entropy_gradient(y_pred, y_true):
     # pega o número de linhas (amostras) que estão sendo processadas
@@ -15,4 +15,5 @@ def cross_entropy_gradient(y_pred, y_true):
     # multiplica o valor da classe verdadeira por 1
     grad[np.arange(n), y_true] -= 1 
     # retorna a matriz com o gradiente calculado para cada amostra
-    return grad
+    #divide por n para o gradiente ser uma média e não uma soma acumulada
+    return grad /n
